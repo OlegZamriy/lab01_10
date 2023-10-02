e67ff9bc465f15216e8c1194470fd57a53030e0c
OlegZamriy <mynameishelg@gmail.com>
2023-10-02 20:02:19 +0300

    1

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-input_list = input("Введіть елементи списку цілих чисел, розділені пробілами: ").split()
-
-input_list = [int(x) for x in input_list]
-
-sum_of_elements = sum(input_list)
-
-average = sum_of_elements / len(input_list)
-
-print(f"Сума всіх елементів: {sum_of_elements}")
-print(f"Середнє арифметичне: {average}")
-
+word = str(input('Введіть текст: '))
+if str (word) == str(word)[::-1]:
+    print('Palindrome')
+else:
+    print('No palidnrome')
